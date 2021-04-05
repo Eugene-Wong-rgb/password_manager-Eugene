d9727c7c07b4c63d42e071d9d3a2aec33f6f173d
Eugene-Wong-rgb <78830586+Eugene-Wong-rgb@users.noreply.github.com>
2021-04-05 17:59:12 +1200

    Update password_manager3.0.py

--- password_manager3.0.py
+++ password_manager3.0.py
@@ -1,36 +1,35 @@
-  while True:
-
-    new_user = input("Enter a username to create a user for a new account: ")
-    x = len(new_user)
-    print(x)
-    if x >= 5:
-      print("Amount of letters in username qualify")
-      user_list.append(new_user)
-      break
-    else:
-      print("Username is too short must contain 5 or more letters")
-
-def password():
-    while True:
-        new_password = input("Please enter a password (must be 8 or higher): ")
-        y = len(new_password)
-        print(y, "is the amount of letters in your pass")
-        if y >= 8:
-            print("password length qualify")
-            password_list.append(new_password)
+#Welcome to password manager. Asks user to make an account for password manager 
+#sub routine
+name = input("Hello, what is your name?")
+while True:
+    try:
+        age = float(input("What is your age?"))
+        if age < 13:
+            print("you do not qualify to make an account")
+            print(" Thank you {} for using pasword manager".format(name))
+        elif age <= 100:
+            print("age meets requirements")
             break
         else:
-            print("Password is too short try again.")   
+            print("please enter a valid age")
+    except ValueError:
+        print("please input valid age")
+print("Welcome to Password Manager. Thank you for using Password Manager! ")
 
 
-
-
-#Main routine - Run main program in a loop
-
-pirnt("Welcome to password manager!")
-print("If you are 13 or older, You can store access details to web or mobile apps")
-
-username = input("enter username to login.")
-password =  int(input("enter password to login."))
-
-
+#sub routine
+while True:
+    try:
+        print("1) Log in to account")
+        print("2) Create an account")
+        mode_1 = float(input("Enter numbers 1 or 2 to chose a mode \n>"))
+    #create account
+        if mode_1 == 2:
+            account = input("New account name \n>")
+            account_password = check_password()
+            
+    #log in to account
+          elif mode_1 == 1:
+              while True:
+                  account = input("What is your account username \n>")
+                  account_password = input("What is your password \n>")
